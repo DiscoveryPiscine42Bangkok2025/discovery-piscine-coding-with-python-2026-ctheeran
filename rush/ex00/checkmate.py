99507fc76cd3fcd7eb5725332b9b0475e8605462
ctheeran <65120018@kmitl.ac.th>
2026-02-12 02:18:48 +0700

    Update checkmate.py

--- rush/ex00/checkmate.py
+++ rush/ex00/checkmate.py
@@ -17,11 +17,12 @@ def is_check(kr, kc, edge, coordinate, type, direction):
 
 
 def checkmate(board):
-    rook = [(0,1), (0,-1), (1,0), (-1,0)]
-    bishop = [(-1,-1), (1,-1), (-1,1), (1,1)]
-    queen = [(0,1), (0,-1), (1,0), (-1,0), (-1,-1), (1,-1), (-1,1), (1,1)]
-    pawn = [(1,-1), (1,1)]
-    piecetype = {"R" : rook, "B" : bishop, "Q" : queen, "P" : pawn}
+    piecetype = {
+        "R" : [(0,1), (0,-1), (1,0), (-1,0)], 
+        "B" : [(-1,-1), (1,-1), (-1,1), (1,1)], 
+        "Q" : [(0,1), (0,-1), (1,0), (-1,0), (-1,-1), (1,-1), (-1,1), (1,1)], 
+        "P" : [(1,-1), (1,1)]
+        }
     coordinate = {}
 
     lines = board.strip().split("\n")
@@ -29,23 +30,27 @@ def checkmate(board):
         for col, piece in enumerate(line):
             if piece in ("K", "R", "Q", "B", "P"):
                 coordinate[(row, col)] = piece
-                if list(coordinate.values()).count("K") > 1:
-                    print("There is more than one King")
-                    return
                 if piece == "K":
-                    kr = row
-                    kc = col
+                    kr, kc = row, col
+    
+    king_count = list(coordinate.values()).count("K")
+    if king_count == 0:
+        print("There is no King")
+        return
+    elif king_count > 1:
+        print("There must be only one King")
+        return
+
+    edge = len(lines)
 
     for line in lines:
-        if not len(line) == len(lines):
+        if not len(line) == edge:
             print("The board isn't a square")
             return
     
-    edge = len(lines)
-
     for type, direction in piecetype.items():
         if is_check(kr, kc, edge, coordinate, type, direction):
             print("Success")
             break
-        else:
-            print("Failed")
+    else:
+        print("Failed")
